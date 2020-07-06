c3f40c5f901d0dac8fc76be7db356b62b2b6a453
Guth <shao34@gmail.com>
2020-07-06 19:19:11 +0200

    Added repositories with base methods and fixed model names.

---
+++ Data/Repositories/TaskRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManager.Data.Models;
+
+namespace TaskManager.Data.Repositories
+{
+    public class TaskRepository : BaseRepository<Models.Task>
+    {
+        public TaskRepository(TaskManagerDBContext dbContext)
+            : base(dbContext)
+        { 
+        }
+    }
+}
